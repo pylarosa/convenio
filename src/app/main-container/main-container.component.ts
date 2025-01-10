48fe13c0077886c64bfd0b9e807af6a360bf7d37
rosenpy <sh1v4n4r4n@gmail.com>
2025-01-10 12:32:38 +0100

    footer: add footer with basci links and social icons

--- src/app/main-container/main-container.component.ts
+++ src/app/main-container/main-container.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {NavbarComponent} from '../navbar/navbar.component';
 import {NgIf} from '@angular/common';
+import {FooterComponent} from '../footer/footer.component';
 
 @Component({
   selector: 'app-main-container',
   imports: [
     RouterOutlet,
     NavbarComponent,
-    NgIf
+    NgIf,
+    FooterComponent
   ],
   templateUrl: './main-container.component.html',
   standalone: true,
