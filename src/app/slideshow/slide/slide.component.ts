3e0ef8ab5482c65d98a6bc19f1228a46a249d832
rosenpy <sh1v4n4r4n@gmail.com>
2025-01-11 14:26:40 +0100

    slideshow: refactor slides logic and adds animation

--- src/app/slideshow/slide/slide.component.ts
+++ src/app/slideshow/slide/slide.component.ts
@@ -1,13 +1,39 @@
-import {Component, input} from '@angular/core';
+import {Component, Input, input, Signal} from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-slide',
   imports: [],
   templateUrl: './slide.component.html',
   standalone: true,
-  styleUrl: './slide.component.css'
+  styleUrl: './slide.component.css',
+  animations: [
+    trigger('slideInAnimation', [
+      state(
+        'inactive',
+        style({
+          transform: 'translateX(100%)',
+          opacity: 0,
+        })
+      ),
+      state(
+        'active',
+        style({
+          transform: 'translateX(0)',
+          opacity: 1,
+        })
+      ),
+      transition('inactive => active', [
+        animate('600ms ease-out'),
+      ]),
+      transition('active => inactive', [
+        animate('400ms ease-in'),
+      ]),
+    ]),
+  ]
 })
 export class SlideComponent {
   image = input.required()
   textInput = input.required()
+  @Input() isActive!: Signal<boolean>;
 }
--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -1,38 +1,64 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, computed, Signal, signal} from '@angular/core';
 import {SlideComponent} from './slide/slide.component';
+import {NgForOf} from "@angular/common";
+
 
 @Component({
   selector: 'app-slideshow',
   imports: [
-    SlideComponent
+    SlideComponent,
+    NgForOf
   ],
   templateUrl: './slideshow.component.html',
   standalone: true,
   styleUrl: './slideshow.component.css'
 })
-export class SlideshowComponent implements OnInit {
-  currentSlide = 0;
-  slides: HTMLElement[] = [];
+export class SlideshowComponent {
+  slides = [
+    {
+      image: '/slides/vacanza.png',
+      text: 'Organizzare la vacanza è più semplice e conveniente \n con offerte che ti fanno partire con il sorriso'
+    },
+    {
+      image: '/slides/ristorante.png',
+      text: 'Il piacere di mangiare fuori incontra il gusto del risparmio. \nBuon cibo, ottime offerte'
+    },
+    {
+      image: '/slides/disco.png',
+      text: 'Rendi le tue serate indimenticabili. \nDivertiti senza pensieri e approfitta delle migliori offerte locali'
+    },
+    {
+      image: '/slides/gastronomia.png',
+      text: 'Scopri sapori autentici e offerte esclusive con Convenio. \nUn’esperienza gastronomica da ricordare.'
+    },
+    {
+      image: '/slides/sauna.png',
+      text: 'Regalati un momento di relax a un prezzo speciale. \nBenessere accessibile a tutti.'
+    },
+    {
+      image: '/slides/cinema.png',
+      text: 'Il cinema e il teatro diventano ancora più accessibili. \nGoditi lo spettacolo, risparmiando'
+    },
+  ];
 
-  ngOnInit() {
-    this.slides = Array.from(document.querySelectorAll('.slide'));
-    this.showSlide(this.currentSlide);
+  currentSlide = signal(0);
 
-    // Automatically change slides every 5 seconds
-    setInterval(() => {
-      this.nextSlide();
+  constructor() {
+    this.startSlideShow();
+  }
+
+  startSlideShow() {
+    const intervalId = setInterval(() => {
+      this.currentSlide.set((this.currentSlide() + 1) % this.slides.length);
+      console.log(this.currentSlide())
     }, 5000);
+
+    // Optionally clean up interval if needed (manual destroy handling)
+    return () => clearInterval(intervalId);
   }
 
-  showSlide(index: number) {
-    this.slides.forEach((slide, i) => {
-      slide.classList.remove('active');
-      if (i === index) slide.classList.add('active');
-    });
-  }
-
-  nextSlide() {
-    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
-    this.showSlide(this.currentSlide);
+  isSlideActive(index: number): Signal<boolean> {
+    console.log(this.currentSlide() === index)
+    return computed(() => this.currentSlide() === index);
   }
 }
