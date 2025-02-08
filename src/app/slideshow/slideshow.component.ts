a499ad1c1bbe4db4077e7fe4176e6b1018be7d14
rosenpy <sh1v4n4r4n@gmail.com>
2025-02-08 18:22:45 +0100

    corrects assets path for deployment

--- src/app/slideshow/slideshow.component.ts
+++ src/app/slideshow/slideshow.component.ts
@@ -16,34 +16,34 @@ import {NgForOf} from "@angular/common";
 export class SlideshowComponent {
   slides = [
     {
-      image: '/slides/vacanza.png',
+      image: 'slides/vacanza.png',
       text: 'Organizzare la vacanza è più semplice e conveniente \n con offerte che ti fanno partire con il sorriso',
       marginBlockEnd: 'low'
     },
     {
-      image: '/slides/ristorante.png',
+      image: 'slides/ristorante.png',
       text: 'Il piacere di mangiare fuori incontra il gusto del risparmio. \nBuon cibo, ottime offerte',
       marginBlockEnd: 'high'
     },
     {
-      image: '/slides/disco.png',
+      image: 'slides/disco.png',
       text: 'Rendi le tue serate indimenticabili. \nDivertiti senza pensieri e approfitta delle migliori offerte locali',
       marginBlockEnd: 'low'
     },
     {
-      image: '/slides/gastronomia.png',
+      image: 'slides/gastronomia.png',
       text: 'Scopri sapori autentici e offerte esclusive con Convenio. \nUn’esperienza gastronomica da ricordare.',
       marginBlockEnd: 'low'
 
     },
     {
-      image: '/slides/sauna.png',
+      image: 'slides/sauna.png',
       text: 'Regalati un momento di relax a un prezzo speciale. \nBenessere accessibile a tutti.',
       marginBlockEnd: 'low'
 
     },
     {
-      image: '/slides/cinema.png',
+      image: 'slides/cinema.png',
       text: 'Il cinema e il teatro diventano ancora più accessibili. \nGoditi lo spettacolo, risparmiando',
       marginBlockEnd: 'low'
     },
